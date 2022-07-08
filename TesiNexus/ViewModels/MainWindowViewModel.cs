d9edf348b74666c4e261dbf3b1b668ea3e2f53f9
tesibrasil <vitor.avila@tesigroup.tech>
2022-07-08 10:03:11 -0300

    Reestruturação de Pastas + Menu Hamburger

--- TesiNexus/ViewModels/MainWindowViewModel.cs
+++ TesiNexus/ViewModels/MainWindowViewModel.cs
@@ -16,10 +16,67 @@ namespace TesiNexus.ViewModels
     {
         public ICommand CloseCommand { get; }
         public ReactiveCommand<object, Unit> EditCommand { get; set; }
+
+
+    #region Hamburger Menu Properties
+        
+        private bool _showMenu;
+
+        public bool ShowMenu
+        {
+            get { return _showMenu; }
+            set
+            {
+                this.RaiseAndSetIfChanged(ref _showMenu, value);
+                ActionMenu();
+
+            }
+        }
+
+        private int _widthMenu;
+
+        public int WidthMenu
+        {
+            get { return _widthMenu; }
+            set
+            {
+                this.RaiseAndSetIfChanged(ref _widthMenu, value);
+
+            }
+        }
+
+        private Thickness _margin;
+
+        public Thickness Margin
+        {
+            get { return _margin; }
+            set
+            {
+                this.RaiseAndSetIfChanged(ref _margin, value);
+            }
+        }
+
+
+        public ReactiveCommand<object, Unit> MouseOverCommand { get; set; }
+        public ReactiveCommand<object, Unit> MouseLeaveCommand { get; set; }
+
+        #endregion
+
+
         public MainWindowViewModel()
         {
             CloseCommand = ReactiveCommand.Create(CloseApp);
             EditCommand = ReactiveCommand.CreateFromTask<object, Unit>(EditCommandExecuted);
+
+            #region Hamburger Menu Initialize
+
+            ShowMenu = false;
+            WidthMenu = 200;
+            Margin = new Thickness(235, 10, 10, 10);
+            MouseOverCommand = ReactiveCommand.CreateFromTask<object, Unit>(MouseOverCommandExecuted);
+            MouseLeaveCommand = ReactiveCommand.CreateFromTask<object, Unit>(MouseLeaveCommandExecuted); 
+
+            #endregion
         }
 
         private void ShowMessage()
@@ -46,8 +103,41 @@ namespace TesiNexus.ViewModels
 
             return Unit.Default;
         }
-        
 
+      #region HamburgerMenuMethods
+        public void ActionMenu()
+        {
+            if (ShowMenu)
+            {
+                WidthMenu = 50;
+                Margin = new Thickness(65, 10, 10, 10);
+            }
+            else
+            {
+                WidthMenu = 200;
+                Margin = new Thickness(235, 10, 10, 10);
+            }
+        }
+
+        private async Task<Unit> MouseOverCommandExecuted(object p)
+        {
+            if (ShowMenu)
+            {
+                WidthMenu = 200;
+            }
+            return Unit.Default;
+        }
+
+        private async Task<Unit> MouseLeaveCommandExecuted(object p)
+        {
+            if (ShowMenu)
+            {
+                WidthMenu = 50;
+            }
+            return Unit.Default;
+        } 
+
+      #endregion
 
     }
 

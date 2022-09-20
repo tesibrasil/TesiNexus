48e8af1be3598dfaa0fb08cd3f11cab758467faa
vitor.avila <vitor.avila@tesigroup.tech>
2022-09-20 10:16:17 -0300

    Criado tabelas ServerGroup e Destination

---
+++ TesiNexus/Synchronizer/Models/Destination.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TesiNexus.Synchronizer.Models
+{
+    public class Destination
+    {
+        public int DestinationID { get; set; }
+        public string Name { get; set; }
+        public string Source { get; set; }
+
+
+        public Destination()
+        {
+            //
+        }
+    }
+}
---
+++ TesiNexus/Synchronizer/Models/ServerGroup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TesiNexus.Synchronizer.Models
+{
+    public class ServerGroup
+    {
+        public int ServerGroupID { get; set; }
+        public String Name { get; set; }
+        public List<Destination> Destinations { get; set; }
+
+
+        public ServerGroup()
+        {
+            //
+        }
+    }
+}
